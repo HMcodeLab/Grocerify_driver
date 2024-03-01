02609fe869ecdffad404bbf380facff2fd3d4f17
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-03-01 17:39:33 +0530

    dashboard complete

--- src/pages/pickedUp/PickedUp.js
+++ src/pages/pickedUp/PickedUp.js
@@ -4,7 +4,7 @@ import Header from "../../components/header/Header";
 import MapImg from "../../assests/images/map.png";
 import DeliverImg from "../../assests/images/deliver.png";
 
-// delivered - pop up done ------- back to orders(my activity)
+// delivered - pop up (done) ------- back to orders(my activity)
 
 const PickedUp = () => {
   const navigate = useNavigate();
